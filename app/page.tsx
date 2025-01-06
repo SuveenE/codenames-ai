054fcaa78ed38e74fc3b03e209eb113af608baef
SuveenE <suveen.te1@gmail.com>
2025-01-05 19:18:54 -0800

    Add environment env

--- app/page.tsx
+++ app/page.tsx
@@ -396,17 +396,19 @@ export default function Home() {
       </div>
       <div className="hidden md:block max-w-7xl mx-auto">
         <div className="relative">
-          <button
-            onClick={replayTestGame}
-            className="fixed right-4 top-4 inline-flex items-center justify-center rounded-xl
-                     bg-gradient-to-r from-indigo-600 to-blue-600 
+          {process.env.NODE_ENV !== "production" && (
+            <button
+              onClick={replayTestGame}
+              className="fixed right-4 top-4 inline-flex items-center justify-center rounded-xl
+                       bg-gradient-to-r from-indigo-600 to-blue-600 
                      px-4 py-2 text-xs font-semibold text-white shadow-sm 
                      hover:from-indigo-500 hover:to-blue-500
                      transition-all duration-200"
-            disabled={isReplaying}
-          >
-            {isReplaying ? "Replaying..." : "Replay Test Game"}
-          </button>
+              disabled={isReplaying}
+            >
+              {isReplaying ? "Replaying..." : "Replay Test Game"}
+            </button>
+          )}
         </div>
         <div className="mb-8 flex items-center flex-row gap-8 justify-center">
           <div className=" space-y-3">
@@ -419,29 +421,44 @@ export default function Home() {
               </p>
             </div>
             <div className="flex gap-4">
-              {!isGameStarted && !isReplaying && (
-                <>
-                  <button
-                    onClick={() => setDialogOpen(true)}
-                    className="inline-flex items-center justify-center rounded-xl
+              {!isGameStarted &&
+                !isReplaying &&
+                process.env.NODE_ENV !== "production" && (
+                  <>
+                    <button
+                      onClick={() => setDialogOpen(true)}
+                      className="inline-flex items-center justify-center rounded-xl
                              bg-gradient-to-r from-indigo-600 to-blue-600 
                              px-4 py-2 text-sm font-semibold text-white shadow-sm 
                              hover:from-indigo-500 hover:to-blue-500
                              transition-all duration-200"
-                  >
-                    Custom
-                  </button>
-                  <button
-                    onClick={handleStartGame}
-                    className="inline-flex items-center justify-center rounded-xl
+                    >
+                      Custom
+                    </button>
+                    <button
+                      onClick={handleStartGame}
+                      className="inline-flex items-center justify-center rounded-xl
                              bg-gradient-to-r from-indigo-600 to-blue-600 
                              px-4 py-2 text-sm font-semibold text-white shadow-sm 
                              hover:from-indigo-500 hover:to-blue-500
                              transition-all duration-200"
-                  >
-                    Start Game
-                  </button>
-                </>
+                    >
+                      Start Game
+                    </button>
+                  </>
+                )}
+              {process.env.NODE_ENV === "production" && (
+                <button
+                  onClick={replayTestGame}
+                  className="inline-flex items-center justify-center rounded-xl
+                             bg-gradient-to-r from-indigo-600 to-blue-600 
+                             px-4 py-2 text-xs font-semibold text-white shadow-sm 
+                             hover:from-indigo-500 hover:to-blue-500
+                             transition-all duration-200 mb-3"
+                  disabled={isReplaying}
+                >
+                  {isReplaying ? "Replaying..." : "Replay Test Game"}
+                </button>
               )}
             </div>
             <CustomGameDialog
